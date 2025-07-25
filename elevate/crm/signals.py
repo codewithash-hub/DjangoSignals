436d710bfc628bf5e60e41e2bf69bc62c4772b43
codewithash <402308182@my.richfield.ac.za>
2025-07-25 10:07:14 +0200

    Adding signals

---
+++ elevate/crm/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Article
+
+@receiver(post_save, sender=Article)
+def article_created(sender, instance, created, **kwargs):
+
+    if created:
+        print(f"New article created: {instance.title}")
